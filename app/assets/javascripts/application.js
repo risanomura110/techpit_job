6750141b484773bd6fca66d4f81139490e0acb0d
risanomura110 <aiueo110boy@gmail.com>
2020-11-12 03:59:20 +0900

    font

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,5 +11,12 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require activestorage
+
 //= require turbolinks
+
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+
 //= require_tree .
