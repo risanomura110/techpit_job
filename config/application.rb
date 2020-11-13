0257ec8e85e225fb3184fe4612cc883a0cdd25b1
risanomura110 <aiueo110boy@gmail.com>
2020-11-14 08:09:40 +0900

    jp

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,8 @@ module TechpitJob
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.time_zone = 'Asia/Tokyo' #この行を追加
-
+    config.time_zone = 'Asia/Tokyo'
+    config.i18n.default_locale = :ja 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
